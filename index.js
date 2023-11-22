a3eb6f0357bab91f35914c51a0dc3658272dd98a
Pencho Ivanov <penchoivanov@Penchos-Air.home>
2023-11-22 21:49:58 +0100

    All done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length){
+      throw new Error("OutOfBounds");
+    } else if(typeof pos === 'number'){
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    let heightstNum = this.items.sort((a,b) => a-b);
 
-  min() {}
+    if(this.items.length === 0){
+      throw new Error("OutOfBounds");
+    } else {
+      return heightstNum[heightstNum.length - 1];
+    }
+  }
 
-  sum() {}
+  min() {
+    let heightstNum = this.items.sort((a,b) => a-b);
 
-  avg() {}
+    if(this.items.length === 0){
+      throw new Error("OutOfBounds");
+    } else {
+      return heightstNum[0];
+    }
+  }
+
+  sum() {
+    if(this.items.length > 0){
+      return this.items.reduce((acc, curValue) => acc + curValue);
+    } else {
+      return 0
+    }
+  }
+
+  avg() {
+    if(this.items.length > 0){
+      const totalNum = this.items.reduce((acc, curValue) => acc + curValue);
+      return totalNum/this.items.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
